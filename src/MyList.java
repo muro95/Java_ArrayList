d9f65570aa88cbed1159e462ce143d881fbc27ec
muro95 <sonbena95@gmail.com>
2019-10-02 08:11:50 +0700

    first commit

---
+++ src/MyList.java
@@ -0,0 +1,115 @@
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object element[] = new Object[DEFAULT_CAPACITY];
+
+    public MyList() {
+
+    }
+
+    public MyList(int capacity) {
+        element = new Object[capacity];
+    }
+
+    public String print() {
+        element = Arrays.copyOf(element, size);
+        return Arrays.toString(element);
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (size == element.length) {
+            int newSize = element.length + minCapacity;
+            element = Arrays.copyOf(element, newSize);
+        }
+    }
+
+    void add(int index, E e ){
+        if (size == 0){
+            ensureCapacity(1);
+            element[size++] = e;
+        } else if(index > size || index < 0){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index );
+        } else {
+            ensureCapacity(1);
+            for (int i = size ; i > index; i--){
+                element[i] = element[i-1];
+            }
+            element[index] = e;
+            size++;
+        }
+    }
+
+    public boolean add(E e) {
+        ensureCapacity(2);
+        element[size++] = e;
+        return true;
+    }
+
+    public E remove(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+        E temp = (E) element[index];
+        for (int i = index; i < size - 1; i++) {
+            element[i] = element[i + 1];
+        }
+        size--;
+        element = Arrays.copyOf(element, size);
+        return temp;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    protected E clone() {
+        Object copyOfElement[] = Arrays.copyOf(element, element.length);
+        return (E) Arrays.toString(copyOfElement);
+    }
+
+    public int indexOf(Object o) {
+        int i;
+        for (i = 0; i < element.length; i++) {
+            if (o == element[i]) {
+                break;
+            }
+        }
+        if (i == element.length) {
+            i = -1;
+        }
+        return i;
+    }
+
+    public boolean contains(Object o) {
+        int i;
+        boolean bo = true;
+        for (i = 0; i < element.length; i++) {
+            if (o == element[i]) {
+                break;
+            }
+        }
+        if (i == element.length) {
+            bo = false;
+        }
+        return bo;
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    public E get(int index) {
+        int i;
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+        for (i = 0; i < element.length; i++) {
+            if (i == index) {
+                break;
+            }
+        }
+        return (E) element[i];
+    }
+}
---
+++ src/MyListTest.java
@@ -0,0 +1,22 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<String> list = new MyList<>(0);
+        list.add(0,"hello");
+        list.add(1,"123");
+        list.add("pp");
+        list.add("p");
+        list.add(2,"-+-+-+-+-+");
+        //System.out.println(list.add("p"));
+        //System.out.println(list.remove(1));
+        System.out.println(list.print());
+        System.out.println(list.size());
+        System.out.println(list.clone());
+        System.out.println(list.indexOf("pp"));
+        System.out.println(list.contains(".."));
+        System.out.println(list.contains("123"));
+        System.out.println(list.get(2));
+        list.clear();
+        System.out.println(list.print());
+
+    }
+}
